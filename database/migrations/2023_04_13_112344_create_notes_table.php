82ef70afc9ea6f03dde37e41644823481d8354b9
naka <114331695+u0r7a1m0@users.noreply.github.com>
2023-04-13 12:25:05 +0000

    [Start]アプリケーション作成

---
+++ database/migrations/2023_04_13_112344_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+            $table->string('name');
+            $table->string('description');
+            $table->string('cord_txt');
+            $table->string('url_txt');
+            
+            $table->integer('user_id');
+            $table->integer('bookmark_id');
+            $table->boolean('public_status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
