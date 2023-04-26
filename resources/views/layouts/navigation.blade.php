3e13b3efb44f8a79755c120e68ef9c71522e6559
naka <114331695+u0r7a1m0@users.noreply.github.com>
2023-04-26 14:22:59 +0000

    画像格納先変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <!--<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />-->
-                        <img src="{{ asset('storage/images/icon.png') }}" width="50" height="50">
+                        <img src="{{ asset('/images/icon.png') }}" width="50" height="50">
                     </a>
                 </div>
                 <!-- Navigation Links -->
